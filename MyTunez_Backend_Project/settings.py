35bce71eda898ea59442afc59b0d895adc5e3069
Kash Grays <kasharsgrays93@.com>
2022-12-08 14:11:48 -0500

    Model Created

--- MyTunez_Backend_Project/settings.py
+++ MyTunez_Backend_Project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'songs'
 ]
 
 MIDDLEWARE = [
@@ -77,13 +78,6 @@ WSGI_APPLICATION = 'MyTunez_Backend_Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
