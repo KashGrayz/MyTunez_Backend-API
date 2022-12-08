d793b4099048d08a9f597a01e4344b942e1d1d9c
Kash Grays <kasharsgrays93@.com>
2022-12-08 16:08:00 -0500

    Created GET+POST for songs_list

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def songs_list(request):
+    if request.method == "GET":
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+        
+    elif request.method == "POST":
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
